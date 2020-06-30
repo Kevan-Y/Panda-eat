7a8f3d9157e69f2f16ca3b1d57908f2befe24d62
Pandanox <58233223+Pandanoxes@users.noreply.github.com>
2020-06-30 12:16:27 -0400

    Adding responsive navbar + fix in the main hero button

---
+++ public/js/nav.js
@@ -0,0 +1,8 @@
+let bar = document.querySelector('#bar');
+bar.addEventListener('click',()=>{
+    if ( document.querySelector('.main-nav').style.display === 'block')
+    document.querySelector('.main-nav').style.display = 'none';
+    else{
+    document.querySelector('.main-nav').style.display = 'block';
+}
+})
