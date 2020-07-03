7a1cec96ba78ca8a21ee3e9fb0e46dec0bfc9987
Pandanox <58233223+Pandanoxes@users.noreply.github.com>
2020-07-03 15:42:54 -0400

    Organize code structure + Adding sending email feature to registration + Small fix in css

---
+++ controller/general.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const router = express.Router();
+const fakeDBS = require("../model/fake-data");
+
+//Home route
+router.get("/", (req, res) => {
+  const fakeDB = new fakeDBS();
+  res.render("general/home", {
+    title: "Panda Feed",
+    style: "main.css",
+    topMeal: fakeDB.getTopMeal(),
+  });
+});
+
+//Register/login route
+router.get("/register", (req, res) => {
+  res.render("general/register", {
+    title: "Login",
+    style: "login.css",
+    register: "none",
+    login: "block",
+  });
+});
+
+//Post for login
+router.post("/login-form", (req, res) => {
+  const error = {};
+  const dataLogins = {
+    email: req.body.emailInfo,
+    password: req.body.passwordInfo,
+  };
+
+  //Print into log
+  console.log(
+    `Login:\n Email: ${req.body.emailInfo}\n Password: ${req.body.passwordInfo}`
+  );
+
+  if (req.body.emailInfo == "") error.invalidEmail = "This field is required.";
+  else {
+    if (!/^\w*@\w*\.\w*$/.test(req.body.emailInfo))
+      error.invalidEmail = "Please enter a valid email address.";
+  }
+  if (req.body.passwordInfo == "")
+    error.invalidPassword = "This field is required.";
+
+  res.render("general/register", {
+    title: "Login",
+    style: "login.css",
+    errorMessageLogin: error,
+    register: "none",
+    login: "block",
+    dataLogin: dataLogins,
+  });
+});
+
+//Post for Register
+router.post("/register-form", (req, res) => {
+  const error = {};
+
+  const dataRegisters = {
+    fistName: req.body.firstInfo,
+    lastName: req.body.lastInfo,
+    email: req.body.emailInfo,
+    password: req.body.passwordInfo,
+  };
+
+  console.log(
+    `Register:\n First Name: ${dataRegisters.fistName}\n Last Name: ${dataRegisters.lastName}\n Email: ${dataRegisters.email}\n Password: ${dataRegisters.password}`
+  );
+
+  if (dataRegisters.fistName == "") error.invalidFirst = "This field is required.";
+  if (dataRegisters.lastName == "") error.invalidLast = "This field is required.";
+  if (dataRegisters.email == "") error.invalidEmail = "This field is required.";
+  else {
+    if (!/^\w*@\w*\.\w*$/.test(dataRegisters.email))
+      error.invalidEmail = "Please enter a valid email address.";
+  }
+
+  if (dataRegisters.password == "")
+    error.invalidPassword = "This field is required.";
+  else {
+    if (!/^[a-zA-Z0-9]{6,12}$/.test(dataRegisters.password))
+      error.invalidPassword =
+        "Please enter a password with between 6-12 characters and must have one letters and numbers only";
+  }
+
+  if (Object.keys(error).length === 0){
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: `${dataRegisters.email}`,
+      from: `Kevanew@hotmail.com`,
+      subject: 'Registration to Panda Feed',
+      html: `<h1>Welcome to Panda Feed</h1><br>
+        Hi ${dataRegisters.fistName} ${dataRegisters.lastName}, you've actived your customer account. 
+      `,
+    };
+    sgMail.send(msg).then(()=>{
+        res.redirect('/');
+    }).catch(err=>{
+        console.log(`Error: ${err}`);
+    });
+
+  }
+  else {
+  res.render("general/register", {
+    title: "Login",
+    style: "login.css",
+    errorMessageRegister: error,
+    register: "block",
+    login: "none",
+    dataRegister: dataRegisters,
+  });
+  }
+  
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const fakeDBS = require("./model/fake-data");
+const bodyParser = require('body-parser');
+
+//load environment key and variable
+require('dotenv').config({path:'./config/keys.env'});
+
+
 const app = express();
 
+//Handlebars middleware
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static("public"));
-app.use(express.urlencoded());
 
-app.get("/", (req, res) => {
-  const fakeDB = new fakeDBS();
-  res.render("home", {
-    title: "Panda Feed",
-    style: "main.css",
-    topMeal: fakeDB.getTopMeal(),
-  });
-});
+//Load controllers
+const generalController = require("./controller/general");
 
+//Map controllers
+app.use("/",generalController);
+
+//Meal-package route
 app.get("/meal-package", (req, res) => {
   const fakeDB = new fakeDBS();
-
   res.render("mealPackge", {
     title: "Meal Package",
     style: "main.css",
@@ -28,86 +34,11 @@ app.get("/meal-package", (req, res) => {
   });
 });
 
-app.get("/register", (req, res) => {
-  res.render("register", {
-    title: "Login",
-    style: "login.css",
-    register: "none",
-    login: "block",
-  });
-});
 
 app.post("/submit-form", (req, res) => {
   console.log(req.body.email + " join Panda Feed");
   res.redirect("/");
 });
 
-app.post("/login-form", (req, res) => {
-  const error = {};
-  const dataLogins = {
-    email: req.body.emailInfo,
-    password: req.body.passwordInfo,
-  };
 
-  //Print into log
-  console.log(
-    `Login:\n Email: ${req.body.emailInfo}\n Password: ${req.body.passwordInfo}`
-  );
-
-  if (req.body.emailInfo == "") error.invalidEmail = "This field is required.";
-  else {
-    if (!/^\w*@\w*\.\w*$/.test(req.body.emailInfo))
-      error.invalidEmail = "Please enter a valid email address.";
-  }
-  if (req.body.passwordInfo == "")
-    error.invalidPassword = "This field is required.";
-
-  res.render("register", {
-    title: "Login",
-    style: "login.css",
-    errorMessageLogin: error,
-    register: "none",
-    login: "block",
-    dataLogin: dataLogins,
-  });
-});
-
-app.post("/register-form", (req, res) => {
-  const error = {};
-  const dataRegisters = {
-    fistName: req.body.firstInfo,
-    lastName: req.body.lastInfo,
-    email: req.body.emailInfo,
-    password: req.body.passwordInfo,
-  };
-
-  console.log(
-    `Register:\n First Name: ${req.body.firstInfo}\n Last Name: ${req.body.lastInfo}\n Email: ${req.body.emailInfo}\n Password: ${req.body.passwordInfo}`
-  );
-
-  if (req.body.firstInfo == "") error.invalidFirst = "This field is required.";
-  if (req.body.lastInfo == "") error.invalidLast = "This field is required.";
-  if (req.body.emailInfo == "") error.invalidEmail = "This field is required.";
-  else {
-    if (!/^\w*@\w*\.\w*$/.test(req.body.emailInfo))
-      error.invalidEmail = "Please enter a valid email address.";
-  }
-  if (req.body.passwordInfo == "")
-    error.invalidPassword = "This field is required.";
-  else {
-    if (!/^[a-zA-Z0-9]{6,12}$/.test(req.body.passwordInfo))
-      error.invalidPassword =
-        "Please enter a password with between 6-12 characters and must have one letters and numbers only";
-  }
-
-  res.render("register", {
-    title: "Login",
-    style: "login.css",
-    errorMessageRegister: error,
-    register: "block",
-    login: "none",
-    dataRegister: dataRegisters,
-  });
-});
-
-app.listen(3000, () => console.log("The web server is up and running"));
+app.listen(process.env.PORT, () => console.log("The web server is up and running"));
